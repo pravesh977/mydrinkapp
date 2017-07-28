1733c1e2fa14d10974fa65178069b9bd0e9b9c66
pravesh <pravesh_shrestha87@yahoo.com>
2017-07-28 00:40:30 -0400

    changing to postgresql

--- drinksapp/db/schema.rb
+++ drinksapp/db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20170719044007) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "commentbody"
-    t.integer "drink_id"
+    t.bigint "drink_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["drink_id"], name: "index_comments_on_drink_id"
@@ -55,4 +58,5 @@ ActiveRecord::Schema.define(version: 20170719044007) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "drinks"
 end
